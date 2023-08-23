0687e14820acc5b8b9b9bf044fffc68cd99f285a
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2023-08-23 16:50:57 -0700

    make recipe work

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -6,22 +6,29 @@ import apache_beam as beam
 import pandas as pd
 
 from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, MergeDim
-from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray, StoreToZarr
+from pangeo_forge_recipes.transforms import (
+    Indexed,
+    OpenURLWithFSSpec,
+    OpenWithXarray,
+    StoreToZarr,
+    T,
+)
 
-dates = pd.date_range("2002-07-04", "2002-07-19", freq="8D")
+dates = pd.date_range("2002-07-04", "2002-07-11", freq="8D")
 variables = ["CHL.chlor_a", "IOP.bbp_443", "SST.sst"]
 
 
-def make_modis_url(date: pd.Timestamp, var: str) -> str:
+def make_modis_url(time: pd.Timestamp, var: str) -> str:
     fmt = "%Y%m%d"
-    end = date + dt.timedelta(days=7)
+    end = time + dt.timedelta(days=7)
     return (
-        "https://oceandata.sci.gsfc.nasa.gov/ob/getfile/AQUA_MODIS."
-        f"{date.strftime(fmt)}_{end.strftime(fmt)}.L3m.8D.{var}.4km.nc"
+        # "https://oceandata.sci.gsfc.nasa.gov/ob/getfile/"
+        "./example-data/"  # for local testing, with data pre-fetched via wget
+        f"AQUA_MODIS.{time.strftime(fmt)}_{end.strftime(fmt)}.L3m.8D.{var}.4km.nc"
     )
 
 
-concat = ConcatDim("date", keys=dates)
+concat = ConcatDim("time", keys=dates)
 merge = MergeDim("var", keys=variables)
 pattern = FilePattern(make_modis_url, concat, merge)
 
@@ -31,15 +38,34 @@ client_kwargs = {
     "trust_env": True,
 }
 
+class Preprocess(beam.PTransform):
+    """Preprocessor transform."""
+
+    @staticmethod
+    def _preproc(item: Indexed[T]) -> Indexed[T]:
+        import numpy as np
+
+        index, ds = item
+        ds = ds.drop("palette")
+        time = np.datetime64(ds.attrs["time_coverage_start"].replace("Z", ""))
+        ds = ds.expand_dims(time=np.array([time]))
+        return index, ds
+
+    def expand(self, pcoll: beam.PCollection) -> beam.PCollection:
+        return pcoll | beam.Map(self._preproc)
+
+
 transforms = (
     beam.Create(pattern.items())
     | OpenURLWithFSSpec(
-        open_kwargs={"client_kwargs": client_kwargs},
-        max_concurrency=1,
+        # open_kwargs={"client_kwargs": client_kwargs},
+        # max_concurrency=1,
     )
     | OpenWithXarray()
+    | Preprocess()
     | StoreToZarr(
         store_name="modis.zarr",
         combine_dims=pattern.combine_dim_keys,
+        target_chunks={"time": 1, "lat": 4320 / 2, "lon": 8640 / 2},
     )
 )
