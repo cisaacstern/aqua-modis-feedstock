9ccba85e8623bedf3ab637a5526b140e9cec9dd3
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2023-08-24 17:01:02 -0700

    rm pangeo_forge_runner_config.py

--- pangeo_forge_runner_config.py
+++
@@ -1,5 +0,0 @@
-c.Bake.bakery_class = "pangeo_forge_runner.bakery.local.LocalDirectBakery"
-c.TargetStorage.fsspec_class = "fsspec.implementations.local.LocalFileSystem"
-c.TargetStorage.root_path = "target"
-c.InputCacheStorage.fsspec_class = "fsspec.implementations.local.LocalFileSystem"
-c.InputCacheStorage.root_path = "cache"
